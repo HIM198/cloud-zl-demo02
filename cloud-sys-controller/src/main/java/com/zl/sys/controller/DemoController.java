bf40883d36f11967bf6299a77a7fe3e3ea2af382
198HIM <1847527975@qq.com>
2023-04-28 00:47:43 +0800

    update two

---
+++ cloud-sys-controller/src/main/java/com/zl/sys/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.zl.sys.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author HIM198
+ * @Date 2023/4/28 0:45
+ * @Description
+ **/
+
+@RestController
+public class DemoController {
+}
